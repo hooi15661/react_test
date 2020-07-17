aed52bd9697918407cf5450f903368a3b7c8ded6
hooi15661 <hooi15661@hotmail.com>
2020-07-17 17:11:08 +0800

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Testing by Chris Hooi
         </p>
         <a
           className="App-link"
