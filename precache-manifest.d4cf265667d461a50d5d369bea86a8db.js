1d4f1a6f53bd2c17237cb1914633cae9db3668b9
hooi15661 <hooi15661@hotmail.com>
2020-07-17 17:17:07 +0800

    Updates

---
+++ precache-manifest.d4cf265667d461a50d5d369bea86a8db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2948976dffc66cf39f47bc0e5cc804fa",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "c1680f4d5734a3a3e329",
+    "url": "/react_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc0ba622b53c301a3420",
+    "url": "/react_test/static/js/2.cb956790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test/static/js/2.cb956790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1680f4d5734a3a3e329",
+    "url": "/react_test/static/js/main.97ee625b.chunk.js"
+  },
+  {
+    "revision": "0acfb237109b04b55a98",
+    "url": "/react_test/static/js/runtime-main.8b15b1c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
